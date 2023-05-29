147be7849813f0e184c15d4ddf1bd9b50988c3fb
DevanshuBhoyar <126349587+DevanshuBhoyar@users.noreply.github.com>
2023-05-29 19:15:10 +0100

    script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yashin";
-names[1]="Jack";
-names[2]="Jim";
-names[3]="jibhe";
-names[4]="pinl";
-names[5]="fault";
-names[6]="lutty";
-names[7]="padsd";
-names[8]="ladfs";
-names[9]="jimxc";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
